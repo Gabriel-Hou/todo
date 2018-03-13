da3c5ab14d39ccf6d6066228cf87cadf0b213a21
gabriel <gabriel@ganguo.hk>
2018-03-13 17:46:31 +0800

    gradle创建-->导包-->发布

--- web/src/main/java/io/ganguo/gradle/todo/App.java
+++ web/src/main/java/io/ganguo/gradle/todo/App.java
@@ -1,11 +1,15 @@
 package io.ganguo.gradle.todo;
 
-import java.util.concurrent.ThreadPoolExecutor;
+
+//import ch.qos.logback.classic.Logger;
+//import org.slf4j.LoggerFactory;
 
 /**
  * Created by Gabriel on 18/3/13.
  **/
 public class App {
+//    static Logger log= LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         TodoItem todo=new TodoItem();
         todo.setId(123);
